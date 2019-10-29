ed18628fa8bdcfeb3da91fe25a3b824e74d4b9c7
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-10-28 22:19:39 -0500

    Prints the titles where keywords are found

--- src/main/java/com/torrecampo/App.java
+++ src/main/java/com/torrecampo/App.java
@@ -13,15 +13,24 @@ public final class App {
     public static void main(String[] args) {
         try {
             String file = args[0];
+            String keyword = args[1];
+            int num = Integer.parseInt(args[2]);
+
+            System.out.println("Filename: " + file);
+            System.out.println("Keyword: " + keyword);
+            System.out.println("n: " + num);
+
             Path path = Paths.get(file);
             Stream<String> lines = Files.lines(path);
-            String keyword = "engineering in industry";
             CharSequence charSequence = new StringBuilder(keyword);
-            // return how many times the keyword is present throughout all titles
-            long count = lines.filter(string -> string.startsWith("#*")).filter(string -> string.contains(charSequence))
-                    .count();
-            System.out.println(count);
-            // Files.write(path, replaced);
+            // return the lines where the keyword is present throughout all titles
+            List<String> filtered = lines.filter(string -> string.startsWith("#*"))
+                    .filter(string -> string.contains(charSequence)).collect(Collectors.toList());
+
+            for (int i = 0; i < filtered.size(); i++) {
+                System.out.println(filtered.get(i));
+            }
+
             lines.close();
         } catch (IOException e) {
             e.printStackTrace();

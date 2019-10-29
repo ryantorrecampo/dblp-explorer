edf18cb37f16acda94eccb9d7c373f8b0d7376e3
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-10-28 22:10:32 -0500

    counts keywords

--- src/main/java/com/torrecampo/App.java
+++ src/main/java/com/torrecampo/App.java
@@ -1,17 +1,30 @@
 package com.torrecampo;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public final class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            String file = args[0];
+            Path path = Paths.get(file);
+            Stream<String> lines = Files.lines(path);
+            String keyword = "engineering in industry";
+            CharSequence charSequence = new StringBuilder(keyword);
+            // return how many times the keyword is present throughout all titles
+            long count = lines.filter(string -> string.startsWith("#*")).filter(string -> string.contains(charSequence))
+                    .count();
+            System.out.println(count);
+            // Files.write(path, replaced);
+            lines.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

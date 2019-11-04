18e2ddb2598e680c29af529b70a7aec9fed01e63
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-11-03 23:50:23 -0600

    check tiers

--- src/main/java/com/torrecampo/App.java
+++ src/main/java/com/torrecampo/App.java
@@ -1,39 +1,70 @@
 package com.torrecampo;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.HashMap;
+import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public final class App {
-
+public class App {
     public static void main(String[] args) {
-        try {
-            String file = args[0];
-            String keyword = args[1];
-            int num = Integer.parseInt(args[2]);
+        String file = args[0];
+        String keyword = args[1];
+        int n = Integer.parseInt(args[2]);
+        Path path = Paths.get(file);
 
-            System.out.println("Filename: " + file);
-            System.out.println("Keyword: " + keyword);
-            System.out.println("n: " + num);
-
-            Path path = Paths.get(file);
-            Stream<String> lines = Files.lines(path);
-            CharSequence charSequence = new StringBuilder(keyword);
-            // return the lines where the keyword is present throughout all titles
-            List<String> filtered = lines.filter(string -> string.startsWith("#*"))
-                    .filter(string -> string.contains(charSequence)).collect(Collectors.toList());
-
-            for (int i = 0; i < filtered.size(); i++) {
-                System.out.println(filtered.get(i));
+        // Ability to reuse the stream
+        Supplier<Stream<String>> streamSupplier = () -> {
+            try {
+                return Files.lines(path);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            return null;
+        };
 
-            lines.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Parse through JSON file
+        JSONParser parser = new JSONParser();
+        List<JSONObject> titles = streamSupplier.get().map(line -> {
+            try {
+                return (JSONObject) parser.parse(line);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+
+        // Finds titles containing the keyword
+        Map<Integer, List<JSONObject>> matches = new HashMap<>();
+        List<JSONObject> first = titles.stream()
+                .filter(item -> item.get("title").toString().toUpperCase().contains(keyword.toUpperCase()))
+                .collect(Collectors.toList());
+        matches.put(0, first);
+
+        // Check n-tiers
+        for (int i = 1; i < n; i++) {
+            List<JSONObject> prevTier = matches.get(i - 1);
+            List<JSONObject> temp = first.stream().filter(item -> item.containsKey("references"))
+                    .filter(ref -> prevTier.contains(ref)).collect(Collectors.toList());
+            for (JSONObject j : prevTier) {
+                String id = j.get("id").toString();
+                first.stream().filter(item -> item.containsKey("references")).filter(ref -> ref.containsValue(id))
+                        .collect(Collectors.toList());
+
+            }
+            matches.put(i, temp);
         }
+        System.out.println(matches.get(0));
+        streamSupplier.get().close();
     }
 }
